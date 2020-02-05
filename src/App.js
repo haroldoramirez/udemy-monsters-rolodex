f3f96091ad93d01566d5f7d3f5cdf623ad474de1
Haroldo Ramirez <haroldoramirez@gmail.com>
2020-02-05 15:00:24 -0300

    aprendendo classes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor() {
+    //chama o construtor do Component
+    super();
+
+    this.state = {
+      string: 'Hello - Haroldo Ramirez da Nobrega'
+    }
+  }
+  render() {
+    return(
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World - Haroldo</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{this.state.string}</p>
+        <button onClick={() => this.setState({ string: 'Hello Haroldo'})}>Change text</button>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//Inserir da DOM a aplicacao App
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
